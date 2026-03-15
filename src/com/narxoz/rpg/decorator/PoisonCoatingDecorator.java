49d07452668e1f788b815da8754504f90bf64061
aruzhankuat <aruzhan.kuat@narxoz.kz>
2026-03-16 00:08:11 +0500

    5

--- src/com/narxoz/rpg/decorator/PoisonCoatingDecorator.java
+++ src/com/narxoz/rpg/decorator/PoisonCoatingDecorator.java
@@ -1,25 +1,21 @@
 package com.narxoz.rpg.decorator;
 
 public class PoisonCoatingDecorator extends ActionDecorator {
+
     public PoisonCoatingDecorator(AttackAction wrappedAction) {
         super(wrappedAction);
     }
 
     @Override
     public String getActionName() {
-        // TODO: Decide how this decorator changes the visible action name.
-        return super.getActionName();
-    }
+        return "Отравленный " + super.getActionName(); }
 
     @Override
     public int getDamage() {
-        // TODO: Add poison-related behavior on top of wrapped damage.
-        return super.getDamage();
-    }
+        return super.getDamage() + 5; }
 
     @Override
     public String getEffectSummary() {
-        // TODO: Append or compose the poison effect description.
-        return super.getEffectSummary();
+        return super.getEffectSummary() + ", Эффект яда";
     }
 }
